595ad10a4739ae3a65e7f149d48b90c1ca9597d1
Dev-Yadnesh8 <142681172+Dev-Yadnesh8@users.noreply.github.com>
2025-01-01 22:22:41 +0530

    app colors added

---
+++ frontend/mobile/learn_x/lib/utils/theme/app_colors.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  
+  static const DARK_BG_COLOR = Color(0xff0d1116);
+  static const ORANGE_COLOR = Color(0xffed892e);
+  static const VOILATE_COLOR = Color(0xff6c77f4);
+  static const LIGHT_BG_COLOR = Color(0xfffbfbfb);
+
+ // TEXT COLORS
+  static const LIGHT_TEXT_COLOR = Color(0xfffbfbfb);
+  static const DARK_TEXT_COLOR = Color(0xff0d1116);
+}
